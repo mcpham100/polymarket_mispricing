36e5c228228b2f37864a2ffecb1b7837ad3c9f32
mcpham100 <phammat2006@gmail.com>
2026-05-20 20:52:44 -0700

    moved detector logic fully into sql to improve compile time on vm

--- run_detector.py
+++ run_detector.py
@@ -3,11 +3,10 @@
 script to run mispricing detection and adding it to table
 '''
 
+# importing libraries
 import pandas as pd
-import time
 import logging
 from polymarket import database_backend as db
-from polymarket import detector
 
 # setup logging similar run_api_calls.py
 logging.basicConfig(filename='detector.log', level=logging.INFO, format='%(asctime)s %(message)s')
@@ -18,26 +17,24 @@ def main():
             conn = db.get_connection() # using existing db logic
             
             # pull snapshots from the db
-            query = "SELECT m.market_id, m.neg_risk, s.yes_price, s.no_price, s.timestamp, s.liquidity " \
+            # filter to only snapshots that are binary (not multi-outcome), liquidity >= 500, and are mispriced
+            # add COUNT over each market to improve compile speed on vm
+            # reduce the size of df significantly when read_sql is executed, preventing out of memory error with 20 million+ rows snapshots table
+            query = "" \
+            "SELECT m.market_id, m.neg_risk, s.yes_price, s.no_price, s.timestamp, s.liquidity, " \
+            "COUNT (*) OVER (PARTITION BY s.market_id) AS num_snapshots, " \
+            "ABS(s.yes_price + s.no_price - 1) as deviation " \
             "FROM snapshots AS s " \
             "JOIN markets AS m ON s.market_id = m.market_id " \
-            "WHERE neg_risk = False AND liquidity >= 500"
+            "WHERE m.neg_risk = False AND s.liquidity >= 500 " \
+            "AND ABS(s.yes_price + s.no_price - 1) > 0.001 "
 
             # create df containing all the snapshots
-            df = pd.read_sql(query, conn)
-            
-            # run detection logic over all the snapshots
-            mispricings = detector.detect_mispricings (df, threshold=0.001)
-            
-            # filter to only be mispriced columns
-            mispricings = mispricings[mispricings['is_mispriced'] == True]
+            # use read_sql_query instead of read_sql to avoid SQLAlchemy warning due to having a psycopg2 connection
+            mispricings = pd.read_sql_query(query, conn)
 
             # ensure mispricings isn't empty to prevent errors when executing other ops
             if not mispricings.empty:
-                # create num_snapshots column
-                # compute num_snapshots per market_id and retain original shape
-                mispricings['num_snapshots'] = mispricings.groupby('market_id')['is_mispriced'].transform('sum')
-
                 # loop through each mispriced event and insert it into mispricing_events
                 for index, event in mispricings.iterrows():
                     db.insert_event(conn, event)
